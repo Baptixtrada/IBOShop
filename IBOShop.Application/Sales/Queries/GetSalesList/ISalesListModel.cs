53a589839f8b5da83b8d4dd570917a4d1101698f
BGGBGIBS <83280831+BGGBGIBS@users.noreply.github.com>
2023-09-01 15:40:24 +0200

    Add:Application:Interfaces

---
+++ IBOShop.Application/Sales/Queries/GetSalesList/ISalesListModel.cs
@@ -0,0 +1,14 @@
+﻿namespace IBOShop.Application.Sales.Queries.GetSalesList
+{
+    public interface ISalesListModel
+    {
+        int Id { get; set; }
+        DateTime Date { get; set; }
+        string CustomerName { get; set; }
+        string EmployeeName { get; set; }
+        string ProductName { get; set; }
+        decimal UnitPrice { get; set; }
+        int Quantity { get; set; }
+        decimal TotalPrice { get; set; }
+    }
+}
