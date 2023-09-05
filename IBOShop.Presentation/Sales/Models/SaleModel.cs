4c1f46705dbef84db87791822daefc46c980a161
baptistegrosjean <grosjeanbaptiste@outlook.com>
2023-09-05 19:16:24 +0200

    Add:Presentation:Sales:DontWork

--- IBOShop.Presentation/Sales/Models/SaleModel.cs
+++ IBOShop.Presentation/Sales/Models/SaleModel.cs
@@ -3,9 +3,10 @@ namespace IBOShop.Presentation.Sales.Models
 {
 	public class SaleModel
 	{
-		public SaleModel()
-		{
-		}
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
 	}
 }
 
--- IBOShop.Presentation/Sales/Services/SaleService.cs
+++ IBOShop.Presentation/Sales/Services/SaleService.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using IBOShop.Presentation.Sales.Models;
+
 namespace IBOShop.Presentation.Sales.Services
 {
 	public class SaleService
 	{
-		public SaleService()
-		{
-		}
-	}
+        private static readonly string[] ProductNames = new[]
+        {
+            "Apple", "Banana", "Cherry", "Orange", "Pear"
+        };
+
+        public Task<SaleModel[]> GetSalesAsync(DateTime startDate)
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new SaleModel
+            {
+                Id = index,
+                ProductName = ProductNames[Random.Shared.Next(ProductNames.Length)],
+                Quantity = Random.Shared.Next(1, 50),
+                Price = Math.Round((decimal)Random.Shared.NextDouble() * 100, 2)
+            }).ToArray());
+        }
+
+    }
 }
 
