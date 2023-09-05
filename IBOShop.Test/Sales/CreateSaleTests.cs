01de13f2a7c46922a9cba6e980d3b24e7f79b7a9
baptistegrosjean <grosjeanbaptiste@outlook.com>
2023-09-05 15:34:01 +0200

    Maj:Refactor:CreateSaleTests

--- IBOShop.Test/Sales/CreateSaleTests.cs
+++ IBOShop.Test/Sales/CreateSaleTests.cs
@@ -14,79 +14,93 @@ using Moq.EntityFrameworkCore;
 
 namespace IBOShop.Test.Sales
 {
-	[TestFixture]
-	public class CreateSaleTests
-	{
-		private CreateSaleCommand _createSaleCommand;
-		private AutoMocker _autoMocker;
-		private CreateSaleModel _createSaleModel;
-		private Sale _sale;
-		private IDateService _dateService;
-		private DateTime _date;
-		private const int _customerId = 1;
-		private const int _employeeId = 2;
-		private const int _productId = 3;
-		private const decimal _unitPrice = 1.23m;
-		private const int _quantity = 4;
+    [TestFixture]
+    public class CreateSaleTests : ICreateSaleTests
+    {
+        private CreateSaleCommand _createSaleCommand;
+        private AutoMocker _autoMocker;
+        private CreateSaleModel _createSaleModel;
+        private Sale _sale;
+        private Customer _customer;
+        private Employee _employee;
+        private Product _product;
+        private int _quantity;
+        private IDateService _dateService;
+        private DateTime _date;
 
-		[SetUp]
-		public void SetUp()
-		{
-			_dateService = new DateService();
-			_date = _dateService.GetDate();
-			Customer customer = new Customer() { Id = _customerId };
-			Employee employee = new Employee() { Id = _employeeId };
-			Product product = new Product() { Id = _productId };
-			_createSaleModel = new CreateSaleModel() { CustomerId = _customerId, EmployeeId = _employeeId, ProductId = _productId, Quantity = _quantity };
-			_sale = new Sale();
-			_autoMocker = new AutoMocker();
-			_autoMocker.GetMock<IDateService>().Setup(p => p.GetDate()).Returns(_date);
-			_autoMocker.GetMock<IDatabaseService>().Setup(p => p.Customers).ReturnsDbSet(new List<Customer> { customer });
-			_autoMocker.GetMock<IDatabaseService>().Setup(p => p.Employees).ReturnsDbSet(new List<Employee> { employee });
-			_autoMocker.GetMock<IDatabaseService>().Setup(p => p.Products).ReturnsDbSet(new List<Product> { product });
-			_autoMocker.GetMock<IDatabaseService>().Setup(p => p.Sales).Returns(_autoMocker.GetMock<DbSet<Sale>>().Object);
-            _autoMocker.GetMock<ISaleFactory>()
-						.Setup(p => p.Create(
-							_date,
-							customer,
-							employee,
-							product,
-							_quantity))
-                        .Returns(_sale);
 
-			_createSaleCommand = _autoMocker.CreateInstance<CreateSaleCommand>();
-		}
-		[Test]
-		public void TestExecuteShouldAddSaleToTheDatabase()
-		{
-			_createSaleCommand.Execute(_createSaleModel);
+        public void Initialize(int customerId, int employeeId, int productId, int quantity)
+        {
+            _customer = new Customer() { Id = customerId };
+            _employee = new Employee() { Id = employeeId };
+            _product = new Product() { Id = productId };
+            _quantity = quantity;
+            _createSaleModel = new CreateSaleModel()
+            {
+                CustomerId = customerId,
+                EmployeeId = employeeId,
+                ProductId = productId,
+                Quantity = quantity
+            };
+            _sale = new Sale();
 
-			_autoMocker.GetMock<DbSet<Sale>>()
-				.Verify(p => p.Add(_sale),
-					Times.Once);
-		}
+        }
 
-		[Test]
-		public void TestExecuteShouldSaveChangesToDatabase()
-		{
-			_createSaleCommand.Execute(_createSaleModel);
+        [SetUp]
+        public void SetUp()
+        {
+            Initialize(1, 2, 3, 4);
+            MockDependencies();
+            _createSaleCommand = _autoMocker.CreateInstance<CreateSaleCommand>();
+        }
 
-			_autoMocker.GetMock<IDatabaseService>()
-				.Verify(p => p.Save(),
-					Times.Once);
-		}
+        private void MockDependencies()
+        {
+            _dateService = new Mock<IDateService>().Object;
+            _date = _dateService.GetDate();
+            _autoMocker = new AutoMocker();
+            _autoMocker.GetMock<IDateService>().Setup(p => p.GetDate()).Returns(_date);
+            _autoMocker.GetMock<IDatabaseService>().Setup(p => p.Customers).ReturnsDbSet(new List<Customer> { _customer });
+            _autoMocker.GetMock<IDatabaseService>().Setup(p => p.Employees).ReturnsDbSet(new List<Employee> { _employee });
+            _autoMocker.GetMock<IDatabaseService>().Setup(p => p.Products).ReturnsDbSet(new List<Product> { _product });
+            _autoMocker.GetMock<IDatabaseService>().Setup(p => p.Sales).Returns(_autoMocker.GetMock<DbSet<Sale>>().Object);
+            _autoMocker.GetMock<ISaleFactory>().Setup(p => p.Create(_date, _customer, _employee, _product, _quantity)).Returns(_sale);
+        }
 
-		[Test]
-		public void TestExecuteShouldNotifyInventoryThatSaleOccurred()
-		{
-			_createSaleCommand.Execute(_createSaleModel);
+        [Test]
+        public void TestExecuteShouldAddSaleToTheDatabase()
+        {
 
-			_autoMocker.GetMock<IInventoryService>()
-				.Verify(p => p.NotifySaleOccured(
-						_productId,
-						_quantity),
-					Times.Once);
-		}
-	}
+            _createSaleCommand.Execute(_createSaleModel);
+
+            _autoMocker.GetMock<DbSet<Sale>>()
+                .Verify(p => p.Add(_sale),
+                    Times.Once);
+        }
+
+        [Test]
+        public void TestExecuteShouldSaveChangesToDatabase()
+        {
+
+            _createSaleCommand.Execute(_createSaleModel);
+
+            _autoMocker.GetMock<IDatabaseService>()
+                .Verify(p => p.Save(),
+                    Times.Once);
+        }
+
+        [Test]
+        public void TestExecuteShouldNotifyInventoryThatSaleOccurred()
+        {
+
+            _createSaleCommand.Execute(_createSaleModel);
+
+            _autoMocker.GetMock<IInventoryService>()
+                .Verify(p => p.NotifySaleOccured(
+                        _product.Id,
+                        _quantity),
+                    Times.Once);
+        }
+    }
 }
 
---
+++ IBOShop.Test/Sales/ICreateSaleTests.cs
@@ -0,0 +1,10 @@
+﻿namespace IBOShop.Test.Sales
+{
+    public interface ICreateSaleTests
+    {
+        void SetUp();
+        void TestExecuteShouldAddSaleToTheDatabase();
+        void TestExecuteShouldNotifyInventoryThatSaleOccurred();
+        void TestExecuteShouldSaveChangesToDatabase();
+    }
+}
